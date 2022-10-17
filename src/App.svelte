3d1157ffcde4188c20075864b18d31347a8650af
Adrian <adrianthelawx@gmail.com>
2022-10-16 18:34:00 -0600

    Primer commit app r&m

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+    let characters = [];
+    let page = 1;
+
+    async function loadCharacters() {
+        const response = await fetch(
+            `https://rickandmortyapi.com/api/character?page=${page}`
+        );
+        const data = await response.json();
+        characters = data.results;
+    }
+    loadCharacters();
+
+    function nextPage() {
+        page++;
+        loadCharacters();
+        console.log("pase")
+    }
+    function previousPage() {
+        page--;
+        loadCharacters();
+    }
+</script>
+
+<main>
+    <div class="title">
+        <h1>Rick And Mory API (svelte)</h1>
+    </div>
+
+    <div class="buttons">
+        <button on:click={previousPage} disabled={page === 1} >Preview</button>
+        <button on:click={nextPage}>Next</button>
+    </div>
+
+    {#each characters as character}
+        <div>
+            <img src={character.image} alt={character.name} />
+            <h2>{character.name}</h2>
+            <p>{character.species}</p>
+        </div>
+    {/each}
+</main>
+
+<style>
+</style>
